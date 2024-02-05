f0720194987afa5afbf6f3cd115b0effab03a114
john-rocky <rockyshikoku@gmail.com>
2024-02-05 13:04:38 +0900

    Initial Commit

---
+++ VisionOSCoreML/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VisionOSCoreML
+//
+//  Created by 間嶋大輔 on 2024/02/05.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ VisionOSCoreML/VisionOSCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSCoreMLApp.swift
+//  VisionOSCoreML
+//
+//  Created by 間嶋大輔 on 2024/02/05.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
